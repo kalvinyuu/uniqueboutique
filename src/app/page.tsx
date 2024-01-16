d1ab6e9ba692605b4b79a231b510f1fccc4f4f8c
kalvin <kalvinyu0@hotmail.com>
2024-01-16 19:21:48 +0000

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,29 +13,35 @@ export default function Home() {
   )
 }
 async function Products() {
-    const products = await getAllProducts();
-    console.log(products)
-  return (
-      <div className="grid">
-      {products.map(product => (
-	    <div className="justify-center products-center">
-		<Link href={`/${product.id}`}> 
-		    <Image
+    try{
+	const products = await getAllProducts();
+	console.log(products)
+	return (
+	    <div className="grid">
+	    {products.map(product => (
+		<div key={product.id} className="justify-center products-center">
+		    <Link href={`/${product.id}`}> 
+			<Image
 			src={`/images/productCatalouge/${product.imageLocation}`}
 			width={500}
 			height={500}
 			alt="Picture of the author"
 
-		    />	
-		    <p>
-			{product.name}
-		    </p>
-		    <p>
-			{product.price} 
-		    </p>
-		</Link>
-	    </div>
-      ))}
+			/>	
+			<p>
+			    {product.name}
+			</p>
+			<p>
+			    {product.price} 
+			</p>
+		    </Link>
+		</div>
+	    ))}
       </div>
-  )
+	)
+    } catch (error) {
+	console.error('Error fetching products:', error);
+	// You might want to handle the error, e.g., display an error message
+	return <div>Error fetching products</div>;
+    }
 } 
