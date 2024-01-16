67028f5bf3bb584f26d2b11c84011179221125d2
kalvin <kalvinyu0@hotmail.com>
2024-01-16 23:42:03 +0000

    still testing usc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import  {Navbar}  from "@/app/components/navbar";
 import React from 'react'
-import CartProvider from './components/providers'
 import { usePathname } from "next/navigation";
 
 export default function RootLayout({
