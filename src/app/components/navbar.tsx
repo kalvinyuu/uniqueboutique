260a94388aca23fe0d999c226142fd38966662bf
kalvin <kalvinyu0@hotmail.com>
2024-01-22 03:25:20 +0000

    will it work

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,27 +4,39 @@ import Image from 'next/image';
 import {createPortal} from "react-dom"
 import ShoppingCart from '@/app/components/cart'
 import {useShoppingCart} from "use-shopping-cart"
-import { SignInButton } from "@clerk/nextjs";
-
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 export function Navbar() {
-  const [showModal2, setShowModal2] = useState(false);
-  const userRef = useRef(null);
-  const {handleCartClick} = useShoppingCart()
-
-  return (
-    <nav className="sticky top-0 w-full flex items-center justify-between p-4 bg-gray-800 text-white mb-8">
-      <div className="text-lg font-bold">Unique Boutique</div>
-      <div>
-	  <SignInButton/>
-        <button 
-	    onClick={() => handleCartClick()}
-            className="px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-600 items-center">
-          <Image src="/cart.svg" width={20} height={20} alt='cart' />
-        </button>
-            <ShoppingCart/>
-      </div>
-    </nav>
-  );
+    const [showModal2, setShowModal2] = useState(false);
+    const userRef = useRef(null);
+    const {handleCartClick} = useShoppingCart()
+    
+    return (
+	<nav className="sticky top-0 w-full flex items-center justify-between p-4 bg-gray-800 text-white mb-8">
+	    <div className="text-lg font-bold">Unique Boutique</div>
+	    <div>
+		<SignedIn>
+		    {/* Mount the UserButton component */}
+		    <UserButton />
+		</SignedIn>
+		<SignedOut>
+		    {/* Signed out users get sign in button */}
+		    <SignInButton/>
+		</SignedOut>
+		<button 
+		    onClick={() => handleCartClick()}
+		    className="px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-600 items-center">
+		    <Image src="/cart.svg" width={20} height={20} alt='cart' />
+		</button>
+		<ShoppingCart/>
+	    </div>
+	</nav>
+    );
 };
 
 
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,58 @@
 import { redirect } from "next/navigation";
 import { checkRole } from "@/../utils/roles";
-
-export default function AdminDashboard() {
-   // If the user does not have the admin role, redirect them to the home page
+import { SearchUsers } from "./_search-users";
+import { clerkClient } from "@clerk/nextjs";
+import { setRole } from "./_actions";
+ 
+export default async function AdminDashboard(params: {
+  searchParams: { search?: string };
+}) {
   if (!checkRole("admin")) {
     redirect("/");
   }
  
+  const query = params.searchParams.search;
+ 
+  const users = query ? await clerkClient.users.getUserList({ query }) : [];
+ 
   return (
     <>
-      <h1>This is the admin dashboard</h1>      
+      <h1>This is the admin dashboard</h1>
       <p>This page is restricted to users with the 'admin' role.</p>
+ 
+      <SearchUsers />
+ 
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <div>
+              {user.firstName} {user.lastName}
+            </div>
+            <div>
+              {
+                user.emailAddresses.find(
+                  (email) => email.id === user.primaryEmailAddressId
+                )?.emailAddress
+              }
+            </div>
+            <div>{user.publicMetadata.role as string}</div>
+            <div>
+              <form action={setRole}>
+                <input type="hidden" value={user.id} name="id" />
+                <input type="hidden" value="admin" name="role" />
+                <button type="submit">Make Admin</button>
+              </form>
+            </div>
+            <div>
+              <form action={setRole}>
+                <input type="hidden" value={user.id} name="id" />
+                <input type="hidden" value="moderator" name="role" />
+                <button type="submit">Make Moderator</button>
+              </form>
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 }
