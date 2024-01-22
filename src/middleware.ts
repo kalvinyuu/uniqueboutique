f6f7011bbde9e3c13b6f75ccf12d74b2cec774d0
kalvin <kalvinyu0@hotmail.com>
2024-01-22 03:38:35 +0000

    little copy and paste error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ["((?!^/admin/).*)"],
+  publicRoutes: ["((?!^/dashboard/).*)"],
 });
  
 export const config = {
