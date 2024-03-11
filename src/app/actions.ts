2d0cd670f55f397abbe53579087b2cbd970ddbcd
kalvin <kalvinyu0@hotmail.com>
2024-03-11 21:30:04 +0000

    wtf it works

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -7,6 +7,7 @@ import { productCatalouge, colour, ribbon, mensSize, womansSize, kidsSize} from 
 import { db } from "@/db/index";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+import crypto from "crypto"
 
 const format = z.object({
     name: z.string().min(1),
@@ -68,26 +69,64 @@ try {
 }
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION!,
+    region: process.env.AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 })
 
-export async function getSignedURL() {
+const allowedFileTypes = [
+  "image/jpeg",
+  "image/png",
+]
 
+const maxFileSize = 1048576 * 10 // 1 MB
+
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString("hex")
+
+type SignedURLResponse = Promise<
+  | { failure?: undefined; success: { url: string; id: number } }
+  | { failure: string; success?: undefined }
+>
+
+type GetSignedURLParams = {
+  fileType: string
+  fileSize: number
+  checksum: string
+}
+export const getSignedURL = async ({
+  fileType,
+  fileSize,
+  checksum,
+}: GetSignedURLParams): SignedURLResponse => {
+
+
+  if (!allowedFileTypes.includes(fileType)) {
+    return { failure: "File type not allowed" }
+  }
+
+  if (fileSize > maxFileSize) {
+    return { failure: "File size too large" }
+  }
+
+  const fileName = generateFileName()
 
   const putObjectCommand = new PutObjectCommand({
-    Bucket: process.env.AWS_BUCKET_NAME!,
-    Key: "test-file",
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: fileName,
+    ContentType: fileType,
+    ContentLength: fileSize,
+    ChecksumSHA256: checksum,
   })
 
   const url = await getSignedUrl(
-    s3Client,
+      s3Client,
     putObjectCommand,
     { expiresIn: 60 } // 60 seconds
   )
 
-  return {success: {url}}
+    console.log({ success: url })
+    
+    return { success: { url, id: 1 } }
 }
